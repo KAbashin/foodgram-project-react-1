6fbd0c40148a4833cb94117131ea7dbabef550d3
Роман <romangrbr@gmail.com>
2022-02-01 21:12:28 +0300

    add api/serializers IngredientQuantity

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from users.models import Follow
+from rest_framework.validators import UniqueTogetherValidator
 
-from .models import Cart, Favorite, Ingredient, Recipe, Tag
+from api.models import Ingredient, IngredientQuantity, Recipe, Tag
+from users.models import Follow
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -23,6 +26,33 @@ class IngredientSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class IngredientQuantitySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = IngredientQuantity
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=IngredientQuantity.objects.all(),
+                fields=['ingredient', 'recipe']
+            )
+        ]
+
+
+class CropRecipeSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+        read_only_fields = ('id', 'name', 'image', 'cooking_time')
+
+
 class FollowSerializer(serializers.ModelSerializer):
     email = serializers.ReadOnlyField(source='author.email')
     id = serializers.ReadOnlyField(source='author.id')
@@ -35,16 +65,21 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follow
-        fields = ('email', 'id', 'username', 'first_name', 'last_name', 
-                  'is_subscribed', 'recipes_count')
+        fields = ('email', 'id', 'username', 'first_name', 'last_name',
+                  'is_subscribed', 'recipes', 'recipes_count')
 
     def get_is_subscribed(self, obj):
         return Follow.objects.filter(
             user=obj.user, author=obj.author
         ).exists()
 
-    def get_recipes(self, abj):
-        return Recipe.objects.filter(author=abj.author)
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        queryset = Recipe.objects.filter(author=obj.author)
+        if limit:
+            queryset = queryset[:int(limit)]
+        return CropRecipeSerializer(queryset, many=True).data
 
-    def get_recipes_count(self, abj):
-        return Recipe.objects.filter(author=abj.author).count()
+    def get_recipes_count(self, obj):
+        return Recipe.objects.filter(author=obj.author).count()
