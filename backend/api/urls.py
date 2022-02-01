56485ac3adfd4ed5df905e515ebeb90d77202d6e
Роман <romangrbr@gmail.com>
2022-02-02 02:02:53 +0300

    changed urls api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientsViewSet, RecipeViewSet, TagsViewSet
+from api.views import IngredientsViewSet, RecipeViewSet, TagsViewSet, UserViewSet
 
 app_name = 'api'
 
@@ -9,7 +9,9 @@ router = DefaultRouter()
 router.register('tags', TagsViewSet)
 router.register('ingredients', IngredientsViewSet)
 router.register('recipes', RecipeViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
