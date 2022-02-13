fca9cdcf61c168b76d47f1db139381af20feb8c9
Роман <romangrbr@gmail.com>
2022-02-13 17:01:00 +0300

    filter is_in_shopping_cart, is_favorited

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -6,30 +6,32 @@ from rest_framework.filters import SearchFilter
 
 
 class TagFavoritShopingFilter(filters.FilterSet):
-    is_in_shopping_cart = filters.BooleanFilter(
-        method="filter_is_in_shopping_cart", widget=BooleanWidget()
-    )
-    is_favorited = filters.BooleanFilter(
-        method="filter_is_favorited", widget=BooleanWidget()
-    )
+    # is_in_shopping_cart = filters.BooleanFilter(
+    #     method="filter_is_in_shopping_cart", widget=BooleanWidget()
+    # )
+    # is_favorited = filters.BooleanFilter(
+    #     method="filter_is_favorited", widget=BooleanWidget()
+    # )
+    is_in_shopping_cart = filters.BooleanFilter(widget=BooleanWidget())
+    is_favorited = filters.BooleanFilter(widget=BooleanWidget())
     tags = AllValuesMultipleFilter(field_name="tags__slug")
     author = AllValuesMultipleFilter(field_name="author__id")
 
-    def filter_is_favorited(self, queryset, name, value):
-        user = self.request.user
-        if value:
-            return Recipe.objects.filter(favorites__user=user)
-        return queryset
+    # def filter_is_favorited(self, queryset, name, value):
+    #     user = self.request.user
+    #     if value:
+    #         return Recipe.objects.filter(favorites__user=user)
+    #     return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        user = self.request.user
-        if value:
-            return Recipe.objects.filter(cart__user=user)
-        return queryset
+    # def filter_is_in_shopping_cart(self, queryset, name, value):
+    #     user = self.request.user
+    #     if value:
+    #         return Recipe.objects.filter(cart__user=user)
+    #     return queryset
 
     class Meta:
         model = Recipe
-        fields = ["tags__slug", "is_favorited", "is_in_shopping_cart"]
+        fields = ["author__id", "tags__slug", "is_favorited", "is_in_shopping_cart"]
 
 
 class IngredientSearchFilter(SearchFilter):
